4e01f9573b37e435dc0076a46f5a55dad90350e4
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2024-08-30 18:53:39 +0100

    commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -19,10 +19,10 @@ const Navbar = () => {
     const [activeLink, setActiveLink] = useState(location.pathname); // Set the initial active link based on the current location
 
     return (
-        <div className='w-full bg-white fixed top-0 left-0 md:px-[135px] z-20'>
+        <div className='w-full bg-white fixed top-0 left-0 z-20'>
             <div className='md:flex items-center bg-white lg:py-7 py-2 md:px-10 px-2'>
                 {/* logo section */}
-                <div className='font-bold cursor-pointer flex items-center gap-1'>
+                <div className='font-bold cursor-pointer gap-1 lg:hidden block'>
                     <span><img src={logo} alt="" className ="lg:w-fit w-40"/></span>
                 </div>
                 {/* Menu icon */}
@@ -30,12 +30,16 @@ const Navbar = () => {
                     {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
                 </div>
                 {/* link items */}
-                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-24 pl-0 transition-all duration-500 ease-in gap-5 justify-center mx-auto ${open ? 'top-20' : 'top-[-490px]'}`}>
+                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-44 pl-0 transition-all duration-500 ease-in gap-5 justify-center mx-auto ${open ? 'top-20' : 'top-[-490px]'}`}>
+                    {/* logo section */}
+                    <div className='font-bold cursor-pointer gap-1 lg:block hidden -ml-48'>
+                        <span><img src={logo} alt="" className ="lg:w-fit w-28"/></span>
+                    </div>
                     {Links.map((link, index) => (
                         <li key={index} className='md:ml-5 md:my-0 my-7 flex flex-col items-center'>
                             <Link 
                                 to={link.link} 
-                                className={`font-semibold text-[16px] ${activeLink === link.link ? 'text-[#8C5B00]' : 'text-[#787070]'}`}
+                                className={`font-semibold text-[16px] ml-3 ${activeLink === link.link ? 'text-[#8C5B00]' : 'text-[#787070]'}`}
                                 onClick={() => setActiveLink(link.link)}
                             >
                                 {link.name}
